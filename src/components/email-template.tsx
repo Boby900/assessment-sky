1bda908a5ede00d9481a724bd7e93d3cd4af9469
boby900 <bobyt2265@gmail.com>
2025-03-03 07:49:38 +0530

    feat: make the form dynamic, started taking values from the inputs

--- src/components/email-template.tsx
+++ src/components/email-template.tsx
@@ -5,7 +5,8 @@ interface EmailTemplateProps {
   export const EmailTemplate = ({ firstName }: EmailTemplateProps) => {
     return (
       <div>
-        <h1>Welcome, {firstName}!</h1>
+        <h1>Welcome, {firstName.split("@")[0]}</h1>
+        <h3>You have subscribed successfully</h3>
       </div>
     );
   };
